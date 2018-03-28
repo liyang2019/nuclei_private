9eaca3d50369632be77b3f1c3a2aa2bdad4a83b8
liyang2034 <liyang2019@gmail.com>
2018-03-28 06:46:24 -0500

    rice cluster submit

--- mask_rcnn/main.py
+++ mask_rcnn/main.py
@@ -64,7 +64,7 @@ if __name__ == '__main__':
         np.random.seed(args.seed)
 
     log = Logger()
-    log.open(args.result_dir + '/log.train.txt', mode='a')
+    log.open(RESULTS_DIR + '/log.train.txt', mode='a')
 
     # net ----------------------
     log.write('** net setting **\n')
