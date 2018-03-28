2ef7839e1d291200f17cd3d7b234389d1e3b031d
liyang2034 <liyang2019@gmail.com>
2018-03-27 22:59:45 -0500

    fix data augmentation

--- mask_rcnn/train_0.py
+++ mask_rcnn/train_0.py
@@ -475,8 +475,9 @@ def run_train():
                     # cv2.imwrite(out_dir + '/train/%s.rcnn_precision.png' % name, all6)
 
                     os.system('rm ' + out_dir + '/train/*')
-                    cv2.imwrite(out_dir + '/train/%05d.%s.rpn_precision.png' % (b, name), all5)
-                    cv2.imwrite(out_dir + '/train/%05d.%s.rcnn_precision.png' % (b, name), all6)
+                    cv2.imwrite(out_dir + '/train/%05d.rpn_precision.png' % b, all5)
+                    cv2.imwrite(out_dir + '/train/%05d.rcnn_precision.png' % b, all6)
+                    cv2.imwrite(out_dir + '/train/%05d.mask_precision.png' % b, all7)
                     # cv2.waitKey(1)
                     pass
 
