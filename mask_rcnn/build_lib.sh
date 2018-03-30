8da40d1fc905c21cd1fa9d299d1d82f243af0a80
liyang2034 <liyang2019@gmail.com>
2018-03-29 19:25:03 -0500

    correct reader to ue new data location

--- mask_rcnn/build_lib.sh
+++ mask_rcnn/build_lib.sh
@@ -8,23 +8,23 @@ ARCH=sm_37
 echo '##########################################################'
 echo '########### building torch NMS ###########################'
 echo '##########################################################'
-TORCH_NMS_DIR=box/nms/torch_nms/
+TORCH_NMS_DIR=net/lib/box/nms/torch_nms/
 nvcc -c -o $TORCH_NMS_DIR'src/nms_kernel.cu.o' $TORCH_NMS_DIR'src/nms_kernel.cu' -x cu -Xcompiler -fPIC -arch=$ARCH
 python $TORCH_NMS_DIR'build.py'
 
 echo '##########################################################'
 echo '########### building roi align pooling layer #############'
 echo '##########################################################'
-ROI_ALIGN_POLL_TF_DIR=roi_align_pool_tf/
+ROI_ALIGN_POLL_TF_DIR=net/lib/roi_align_pool_tf/
 nvcc -c -o $ROI_ALIGN_POLL_TF_DIR'src/crop_and_resize_kernel.cu.o' $ROI_ALIGN_POLL_TF_DIR'src/crop_and_resize_kernel.cu' -x cu -Xcompiler -fPIC -arch=$ARCH
-python roi_align_pool_tf/build.py
+python $ROI_ALIGN_POLL_TF_DIR/build.py
 
 # build the cython *.so lib for your system
 
 echo '##########################################################'
 echo '########### building cython NMS ##########################'
 echo '##########################################################'
-cd box/nms/cython_nms
+cd net/lib/box/nms/cython_nms
 python setup.py build_ext --inplace
 
 echo '##########################################################'
