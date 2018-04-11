2c2eedf6da23a4d89b7a4b3e83e3a1d9f542710f
liyang2034 <liyang2019@gmail.com>
2018-04-10 19:11:16 -0500

    mask mini unet

--- mask_rcnn/net/model.py
+++ mask_rcnn/net/model.py
@@ -415,8 +415,8 @@ class MaskNet(nn.Module):
         self.rcnn_crop = CropRoi(cfg, cfg.rcnn_crop_size)
         self.rcnn_head = RcnnHead(cfg, crop_channels)
         self.mask_crop = CropRoi(cfg, cfg.mask_crop_size)
-        self.mask_head = MaskHead(cfg, crop_channels)
-        # self.mask_head = MaskHeadMiniUnet(cfg, crop_channels)
+        # self.mask_head = MaskHead(cfg, crop_channels)
+        self.mask_head = MaskHeadMiniUnet(cfg, crop_channels)
 
         if USE_CUDA:
             print('in constructing mask rcnn net, using gpu, using data parallel')
