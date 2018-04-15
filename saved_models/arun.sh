75f6e463eaeaa81a8053740c9339ae691b2819ec
liyang2034 <liyang2019@gmail.com>
2018-04-15 15:39:29 -0500

    ensemble predict

--- saved_models/arun.sh
+++ saved_models/arun.sh
@@ -3,35 +3,35 @@ export PYTHONPATH=../:../mask_rcnn:../ensemble
 python predict.py \
     --initial_checkpoint ../results/2018-4-14_gray800_box/2018-04-15_03-47-56/checkpoint/00026500_model.pth \
     --test_augment_mode none \
-    --result_dir a020_2018-4-15_models_gray690_00037500_model \
+    --result_dir 2018-4-15_models_gray800_00026500_model.pth \
     >> log.txt
 
 python predict.py \
     --initial_checkpoint ../results/2018-4-14_gray800_box/2018-04-15_03-47-56/checkpoint/00026500_model.pth \
     --test_augment_mode hflip \
-    --result_dir a020_2018-4-15_models_gray690_00037500_model \
+    --result_dir 2018-4-15_models_gray800_00026500_model.pth \
     >> log.txt
 
 python predict.py \
     --initial_checkpoint ../results/2018-4-14_gray800_box/2018-04-15_03-47-56/checkpoint/00026500_model.pth \
     --test_augment_mode vflip \
-    --result_dir a020_2018-4-15_models_gray690_00037500_model \
+    --result_dir 2018-4-15_models_gray800_00026500_model.pth \
     >> log.txt
 
 python predict.py \
     --initial_checkpoint ../results/2018-4-14_gray800_box/2018-04-15_03-47-56/checkpoint/00026500_model.pth \
     --test_augment_mode scaleup \
-    --result_dir a020_2018-4-15_models_gray690_00037500_model \
+    --result_dir 2018-4-15_models_gray800_00026500_model.pth \
     >> log.txt
 
 python predict.py \
     --initial_checkpoint ../results/2018-4-14_gray800_box/2018-04-15_03-47-56/checkpoint/00026500_model.pth \
     --test_augment_mode scaledown \
-    --result_dir a020_2018-4-15_models_gray690_00037500_model \
+    --result_dir 2018-4-15_models_gray800_00026500_model.pth \
     >> log.txt
 
 python predict.py \
     --initial_checkpoint ../results/2018-4-14_gray800_box/2018-04-15_03-47-56/checkpoint/00026500_model.pth \
     --test_augment_mode blur \
-    --result_dir a020_2018-4-15_models_gray690_00037500_model \
+    --result_dir 2018-4-15_models_gray800_00026500_model.pth \
     >> log.txt
